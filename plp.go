859c6b2238556cbc26a69d9f556fd81678e27e7a
Rogerio Maia <rogeriomql@gmail.com>
2020-05-14 17:23:24 -0300

    metodo para obter digito verificador

--- plp.go
+++ plp.go
@@ -177,86 +177,6 @@ func (p *Plp) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// Valida valida dados do remetente
-func (p *Plp) Valida(idCorreios string) error {
-	if !erCartao.MatchString(p.Plp.CartaoPostagem) {
-		return ErrCartaoInvalido
-	}
-	if !erCartao.MatchString(p.Remetente.NumeroContrato) {
-		return ErrContratoInvalido
-	}
-	if !erDr.MatchString(p.Remetente.NumeroDiretoria) {
-		return ErrDrInvalido
-	}
-	if !erCodAdm.MatchString(p.Remetente.CodigoAdministrativo) {
-		return ErrCodAdmInvalido
-	}
-
-	if p.Remetente.NomeRemetente.CData == "" || len(p.Remetente.NomeRemetente.CData) > 50 {
-		return ErrNomeRemetente
-	}
-	if p.Remetente.LogradouroRemetente.CData == "" || len(p.Remetente.LogradouroRemetente.CData) > 50 {
-		return ErrLogradouroRemetente
-	}
-	if p.Remetente.NumeroRemetente.CData == "" || len(p.Remetente.NumeroRemetente.CData) > 5 {
-		return ErrNumeroRemetente
-	}
-	if len(p.Remetente.ComplementoRemetente.CData) > 30 {
-		return ErrComplementoRemetente
-	}
-	if p.Remetente.BairroRemetente.CData == "" || len(p.Remetente.BairroRemetente.CData) > 30 {
-		return ErrBairroRemetente
-	}
-	if !erCep.MatchString(p.Remetente.CepRemetente.CData) {
-		return ErrCepRemetente
-	}
-	if p.Remetente.CidadeRemetente.CData == "" || len(p.Remetente.CidadeRemetente.CData) > 30 {
-		return ErrCidadeRemetente
-	}
-	if !erUf.MatchString(p.Remetente.UfRemetente) {
-		return ErrUfRemetente
-	}
-	if p.Remetente.TelefoneRemetente.CData != "" && !erTelefone.MatchString(p.Remetente.TelefoneRemetente.CData) {
-		return ErrTelefoneRemetente
-	}
-	if p.Remetente.TelefoneRemetente.CData != "" && len(p.Remetente.TelefoneRemetente.CData) > 12 {
-		return ErrTelefoneRemetente
-	}
-	if p.Remetente.EmailRemetente.CData != "" && !erEmail.MatchString(p.Remetente.EmailRemetente.CData) {
-		return ErrEmailRemetente
-	}
-	if p.Remetente.CelularRemetente.CData != "" && !erTelefone.MatchString(p.Remetente.CelularRemetente.CData) {
-		return ErrCelularRemetente
-	}
-	/*cartao, err := cc.Cartao(p.Plp.CartaoPostagem)
-	if err != nil {
-		return fmt.Errorf("plp valida 1: %s", err)
-	}
-	if cartao.Status != "01" {
-		return fmt.Errorf("info: plp valida 2: cartão de postagem %q não está ativo", p.Plp.CartaoPostagem)
-	}
-	contratosPorNumeroDr, err := cc.Contratos("", p.Remetente.NumeroContrato, p.Remetente.NumeroDiretoria)
-	if err != nil {
-		return fmt.Errorf("plp valida 3: %s", err)
-	}
-	valido := false
-	for _, c := range contratosPorNumeroDr {
-		if c.Status == "A" {
-			valido = true
-			break
-		}
-	}
-	if !valido {
-		return fmt.Errorf(
-			"info: plp valida 4: não existe contrato ativo com número %q e diretoria %q",
-			p.Remetente.NumeroContrato,
-			p.Remetente.NumeroDiretoria,
-		)
-	}
-	*/
-	return nil
-}
-
 // XML monta o xml da PLP
 func (p *Plp) XML() (string, error) {
 	replacer := strings.NewReplacer(
@@ -364,128 +284,10 @@ func NewPlp(b []byte) (Plp, error) {
 	return plp, err
 }
 
-//FindXML busca uma PLP no banco tendo o numero da PLP como parâmetro de entrada
-func FindXML(numero int) (string, error) {
-	query := `
-		SELECT	PLP_IN_STATUS, PLP_TX_XML, PLP_TX_XML_RETORNO
-		FROM NEP_PRE_LISTA_POSTAGEM
-		WHERE PLP_NU = :numero	
-	`
-	var (
-		plpInStatus  int
-		txXML        string
-		txXMLRetorno string
-	)
-	row := db.QueryRow(query, numero)
-	if err := row.Scan(
-		&plpInStatus,
-		&txXML,
-		&txXMLRetorno,
-	); err != nil {
-		return "", fmt.Errorf("plp PlpFindXML 1: %s", err)
-		//PLP não encontrada
-		//return "", nil
-	}
-
-	if txXMLRetorno != "" {
-		return txXMLRetorno, nil
-	}
-	return txXML, nil
-}
-
-//Disponibilidade retorna a quantidade de dias que uma PLP fica disponível para postagem
-func Disponibilidade() (int, error) {
-	query := `
-		SELECT PRM_TE_VALOR FROM NEP_PARAMETRO_PLP WHERE PRM_TX_DESCRICAO = 'DisponibilidadePLP'
-	`
-	var dias int
-	row := db.QueryRow(query)
-	if err := row.Scan(&dias); err != nil {
-		return 0, fmt.Errorf("plp disponibilidade: %s", err)
-	}
-	return dias, nil
-}
-
-//AtualizaXML grava alterações realizadas na PLP diretamente no Banco de Dados
-func (p *Plp) AtualizaXML() error {
-	tx, err := db.Begin()
-	docXML, err := p.XML()
-	if err != nil {
-		return fmt.Errorf("plp atualiza xml 1: %s", err)
-	}
-	query := `UPDATE NEP_PRE_LISTA_POSTAGEM SET PLP_TX_XML = :docXML WHERE PLP_NU = :id`
-	_, err = tx.Exec(query, docXML, p.Plp.IDPlp)
-	if err != nil {
-		if rollbackErr := tx.Rollback(); rollbackErr != nil {
-			return rollbackErr
-		}
-		return fmt.Errorf("plp atualizaXML 2: oracle: %s", err)
-	}
-	tx.Commit()
-	return nil
-}
-
-//ObtemListaPLPSIntactas retorna uma lista de PLPS de um determinado contrato que não sofreram manipulações em nenhum sistema
-func ObtemListaPLPSIntactas(contrato string, dias int) ([]int, error) {
-	var plps []int
-	query := `
-		SELECT   /*+ PARALLEL(32) */
-		NEP_PreLstPtg.PLP_nu		
-		FROM          NEP_Pre_Lista_Postagem NEP_PreLstPtg
-		JOIN Vw_F5536B              VwF5536B
-		ON (  VwF5536B.ITZIACPJDE
-		= NEP_PreLstPtg.CPZIACPJDE)
-		WHERE    (    (  trim(VwF5536B.ITDMCT)
-		= :contrato                                 ) -- <---<<< Informe o número do contrato.
-		AND (        NEP_PreLstPtg.PLP_in_Incorporada
-		<>      'S'                                    )
-		AND NOT EXISTS(SELECT 1
-		FROM   NEP_Objeto_Postal NEP_ObjPst
-			WHERE  (    (  NEP_ObjPst.PLP_nu
-							= NEP_PreLstPtg.PLP_nu)
-					AND (  NEP_ObjPst.Obj_in_Status
-							= 1                   ))) -- 1 - Postado.
-		AND (        NEP_PreLstPtg.PLP_dt_Postagem
-		BETWEEN (  TRUNC(SYSDATE)
-			- :dias)
-		AND     (TRUNC(SYSDATE) + 86399/86400) ))
-		GROUP BY  NEP_PreLstPtg.PLP_nu`
-
-	rows, err := db.Query(query, contrato, dias)
-	if err != nil {
-		return plps, err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var plp int
-		err := rows.Scan(&plp)
-		if err != nil {
-			return plps, fmt.Errorf("erro ao encontrar plps do contrato: %s", err)
-		}
-		plps = append(plps, plp)
-	}
-	if len(plps) == 0 {
-		return plps, errors.New("nenhuma plp intacta foi localizada para o contrato: " + contrato)
-	}
-	return plps, nil
-
-}
-
 //IsoUtf8 converte de ISO para UTF-8
 func IsoUtf8(b []byte) ([]byte, error) {
 	r := charmap.ISO8859_1.NewDecoder().Reader(strings.NewReader(string(b)))
 	return ioutil.ReadAll(r)
-}
-
-//estrutura para conter o numero de uma PLP
-type fechaPlpVariosServicosResponse struct {
-	XMLName xml.Name `xml:"Envelope"`
-	Body    struct {
-		XMLName                        xml.Name
-		FechaPlpVariosServicosResponse struct {
-			NumeroPLP string `xml:"return"`
-		} `xml:"fechaPlpVariosServicosResponse"`
-	}
 }
 
 //estrutura para tratar requests que tiverem erro
@@ -500,6 +302,67 @@ type fault struct {
 	} `xml:"Body"`
 }
 
+//estrutura para conter o retorno do método geraDigitoVeiricadorEtiquetas
+type geraDigitoVerificadorEtiquetasResponse struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		XMLName                                xml.Name
+		GeraDigitoVerificadorEtiquetasResponse struct {
+			DigitoVerificador int `xml:"return"`
+		} `xml:"geraDigitoVerificadorEtiquetasResponse"`
+	}
+}
+
+//GeraDigitoVerificadorEtiquetas faz a chamada ao SIGPEWEB e gera o dígito verificador de uma etiqueta
+func GeraDigitoVerificadorEtiquetas(wsdl string, etiqueta string) (int, error) {
+	payload := fmt.Sprintf(
+		`<x:Envelope xmlns:x="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cli="http://cliente.bean.master.sigep.bsb.correios.com.br/">
+		 <x:Header/>
+		 <x:Body>
+			<cli:geraDigitoVerificadorEtiquetas>
+				<etiquetas>` + etiqueta + `</etiquetas>
+				<usuario>gati</usuario>
+				<senha>lbqhj</senha>
+			</cli:geraDigitoVerificadorEtiquetas>
+		</x:Body>
+		</x:Envelope>
+		`)
+	req, err := http.NewRequest("POST", wsdl, strings.NewReader(payload))
+	if err != nil {
+		return 99, err
+	}
+	http.DefaultClient.Transport = &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 99, err
+	}
+	b, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return 99, err
+	}
+	b, err = IsoUtf8(b)
+	if strings.Contains(string(b), "faultstring") {
+		respError := fault{}
+		_ = xml.Unmarshal([]byte(b), &respError)
+		return 99, errors.New(respError.Body.Fault.FaultString)
+	}
+	digito := geraDigitoVerificadorEtiquetasResponse{}
+	_ = xml.Unmarshal([]byte(b), &digito)
+	return digito.Body.GeraDigitoVerificadorEtiquetasResponse.DigitoVerificador, nil
+}
+
+//estrutura para conter o numero de uma PLP
+type fechaPlpVariosServicosResponse struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		XMLName                        xml.Name
+		FechaPlpVariosServicosResponse struct {
+			NumeroPLP string `xml:"return"`
+		} `xml:"fechaPlpVariosServicosResponse"`
+	}
+}
+
 //FechaPlpVariosServicos faz a chamada ao SIGPEWEB, fecha uma PLP
 func FechaPlpVariosServicos(wsdl string) (string, error) {
 	payload := fmt.Sprintf(
@@ -510,7 +373,7 @@ func FechaPlpVariosServicos(wsdl string) (string, error) {
 					<!--Optional:-->
 					<xml><![CDATA[<?ml version="1.0" encoding="ISO-8859-1"?><correioslog><tipo_arquivo>Postagem</tipo_arquivo><versao_arquivo>2.3</versao_arquivo><plp><id_plp /><valor_global/><mcu_unidade_postagem/><nome_unidade_postagem/><cartao_postagem>0068600275</cartao_postagem></plp><remetente><numero_contrato>9912208555</numero_contrato><numero_diretoria>10</numero_diretoria><codigo_administrativo>08082650</codigo_administrativo><nome_remetente>Monitor de Fechamento de PLP</nome_remetente><logradouro_remetente>SNQ Quadra 1 Bloco A 2º SS</logradouro_remetente><numero_remetente>0</numero_remetente><complemento_remetente/><bairro_remetente>Asa Norte</bairro_remetente><cep_remetente>70002900</cep_remetente><cidade_remetente>Brasília</cidade_remetente><uf_remetente>DF</uf_remetente><telefone_remetente>6121416129</telefone_remetente><fax_remetente/><email_remetente/></remetente><forma_pagamento/><objeto_postal><numero_etiqueta>OH566322999BR</numero_etiqueta><codigo_objeto_cliente/><codigo_servico_postagem>04162</codigo_servico_postagem><cubagem>0,0000</cubagem><peso>800</peso><rt1/><rt2/><destinatario><nome_destinatario>Correios DETEC</nome_destinatario><telefone_destinatario>6121416129</telefone_destinatario><celular_destinatario/><email_destinatario/><logradouro_destinatario>SNN Quadra 1 Bloco A</logradouro_destinatario><complemento_destinatario/><numero_end_destinatario>0</numero_end_destinatario></destinatario><nacional><bairro_destinatario>Asa Norte</bairro_destinatario><cidade_destinatario>Brasília</cidade_destinatario><uf_destinatario>DF</uf_destinatario><cep_destinatario>70002900</cep_destinatario><codigo_usuario_postal/><centro_custo_cliente/><numero_nota_fiscal>1234567</numero_nota_fiscal><serie_nota_fiscal/><valor_nota_fiscal/><natureza_nota_fiscal/><descricao_objeto/><valor_a_cobrar>0,0</valor_a_cobrar></nacional><servico_adicional><codigo_servico_adicional>025</codigo_servico_adicional><valor_declarado/></servico_adicional><dimensao_objeto><tipo_objeto>002</tipo_objeto><dimensao_altura>50</dimensao_altura><dimensao_largura>30</dimensao_largura><dimensao_comprimento>40</dimensao_comprimento><dimensao_diametro>0</dimensao_diametro></dimensao_objeto><data_postagem_sara/><status_processamento>0</status_processamento><numero_comprovante_postagem/><valor_cobrado/></objeto_postal></correioslog>]]></xml>
 					<!--Optional:-->
-					<idPlpCliente>54321</idPlpCliente>
+					<idPlpCliente>05052020</idPlpCliente>
 					<!--Optional:-->
 					<cartaoPostagem>0068600275</cartaoPostagem>
 					<!--Zero or more repetitions:-->
